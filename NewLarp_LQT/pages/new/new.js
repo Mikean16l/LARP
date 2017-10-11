771578b92d2ef681ab73f67240d81ae4d91d44dc
Mikean16l <mikean16l@gmail.com>
2017-10-12 06:21:05 +0800

    Last version be4 using Jianan's

--- NewLarp_LQT/pages/new/new.js
+++ NewLarp_LQT/pages/new/new.js
@@ -7,19 +7,19 @@ Page({
       cardUrl: '../../imagesBeta/1.jpg', //or 'http://.png'
       cardInfo: {
         cardTitle: 'Mystery Express',
-        cardInfoMes: ['在前往伦敦的特快列车上，神秘的乘客', '密闭的包厢内，富豪已死去多时', '你必须配合侦探洗脱嫌疑，请找出真凶']
+        cardInfoMes: ['前往伦敦的特快列车，神秘的乘客', '密闭的包厢内，富豪已死去多时', '你必须洗脱嫌疑，请找出真凶']
       }
     }, {
         cardUrl: '../../imagesBeta/2.jpg',
       cardInfo: {
         cardTitle: 'Cargo Noir',
-        cardInfoMes: ['在豪华游轮黑玫瑰号上，达官显贵齐聚于此', '神秘的vip咖啡厅，两名受害人', '你必须完成自己的任务，在炸弹爆炸前逃离']
+        cardInfoMes: ['豪华游轮黑玫瑰号，达官显贵齐聚', '神秘的vip咖啡厅，两名受害人', '必须完成任务，在炸弹爆炸前逃离']
       }
     }, {
         cardUrl: '../../imagesBeta/3.jpg',
       cardInfo: {
         cardTitle: 'LARP',
-        cardInfoMes: ['LARP的参与者在用肢体扮演虚构人物，', '即兴诠释他们角色的说话和动作，', '参与到各种情景与谜题当中，完成任务']
+        cardInfoMes: ['LARP参与者用肢体扮演虚构人物，', '即兴诠释他们角色的说话和动作，', '参与各种情景谜题中，完成任务']
       }
     }]
   },
